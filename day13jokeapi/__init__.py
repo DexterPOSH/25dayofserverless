ec108fad7b3150d6a2f2f91079b639abb5204659
DexterPOSH <dexter34925@yahoo.in>
2019-12-14 18:18:40 +0530

    Adds the Joke API & trained weights file

---
+++ day13jokeapi/__init__.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import pathlib
+from textgenrnn import textgenrnn
+import azure.functions as func
+
+logging.info(os.getcwd())
+weightsFile = pathlib.Path(__file__).parent / 'textgenrnn_weights.hdf5'
+TEXTGEN = textgenrnn(weightsFile)
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    joke = newJoke()
+    return func.HttpResponse(
+        joke[0],
+        status_code=200,
+        headers= {
+            'Content-Type': 'text/html'
+        }
+    )
+
+
+def newJoke(temp : float = 0.5) -> str:
+    global TEXTGEN
+    return TEXTGEN.generate(temperature=temp, return_as_list=True)
