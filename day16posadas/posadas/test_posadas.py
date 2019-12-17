58fa969800379445b354e80eb232a72a354662b8
DexterPOSH <dexter34925@yahoo.in>
2019-12-17 19:28:59 +0530

    Fixes the test

--- day16posadas/posadas/test_posadas.py
+++ day16posadas/posadas/test_posadas.py
@@ -7,10 +7,10 @@ def test_posadas_exists():
     posadasJsonFile = pathlib.Path(__file__).parent / "posadas.json"
     assert posadasJsonFile.exists() == True
 
-def assert_valid_schema():
+def test_valid_schema():
     schemaFilePath = pathlib.Path(__file__).parent / "posadas.schema.json"
     schema = json.loads(schemaFilePath.read_text())
     posadasJsonFile = pathlib.Path(__file__).parent / "posadas.json"
     data = json.loads(posadasJsonFile.read_text())
-    return validate(data, schema)
+    validate(data, schema)
 
